377398d2d3dd67238d9d9cdc433d4f3bba9323d2
Lucien Blunk-Lallet <lucien.blunk.lallet@gmail.com>
2020-05-22 00:27:26 +0200

    chore: moved bot token to env config

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const client = new Discord.Client();
 const Server = require("./src/core/Server");
 const Metadata = require("./src/utils/Metadata");
 
+require('dotenv').config();
+
 client.on('message', async msg => {
     if (msg.author.bot) return;
     if (!msg.channel.guild) return;
@@ -20,11 +22,9 @@ client.on('message', async msg => {
         await msg.author.send(Metadata.generate(metadata));
         await msg.author.send(`Dear ${msg.author.username},\nYour ticket has successfully been created. Your ticket ID is ${ticket}.\nYou can now write a message, an agent will answer you as soon as possible.`)
     }
-
-    //io.emit('chat message', { author: msg.author.username, content: msg.content });
 });
 
-client.login('NjQzNDEyNTI0NjQ1MjIwMzcz.Xr_m6g.47qoTcggD8O9QcR5KosWNqMIIfs').then(() => {
+client.login(process.env.BOT_TOKEN).then(() => {
     console.log(`Discord bot connected as ${client.user.tag}!`);
     new Server(client).start();
 });
